7803520688358393549787e09a6d2317667c7dd0
Luis Bernal <lbernal@dynatronsoftware.com>
2022-01-28 22:36:08 -0600

    Add project files.

---
+++ DAPracticeCSharp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DAPracticeCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a string");
+            string input = Console.ReadLine();
+            ReverseAString(input);
+            Console.Read();
+        }
+        public static void ReverseAString(string str)
+        {
+            string result = "";
+            for(var i = 0;i<str.Length;i++)
+            {
+                result = str[i] + result;  
+                
+            }
+            Console.WriteLine(result);
+        }
+    }
+
+  
+}
+
